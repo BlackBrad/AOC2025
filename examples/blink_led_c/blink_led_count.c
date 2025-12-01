2b2494ad2fa6e951ebb204c226eaa1161cd55f56
Bradley Black <bradley_black@icloud.com>
2025-12-01 22:46:54 +1300

    Make more blinky

--- examples/blink_led_c/blink_led_count.c
+++ examples/blink_led_c/blink_led_count.c
@@ -25,8 +25,25 @@ int main(void){
                 gpio_put(3, 1);
             if (counter & 0x08)
                 gpio_put(2, 1);
-            sleep_ms(1000);
+            sleep_ms(250);
             reset_all_leds();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int i = 2; i < 6; i++) {
+                gpio_put(i, 1);
+                sleep_ms(150);
+                reset_all_leds();
+                sleep_ms(150);
+            }
+        }
+        for (int i = 0; i < 20; i++) {
+            for (int i = 2; i < 6; i++) {
+                gpio_put(i, 1);
+            }
+            sleep_ms(50);
+            reset_all_leds();
+            sleep_ms(50);
         }
     }
 
